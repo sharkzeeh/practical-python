e45255ab41a4059e2fb5ed73eea50f7ec262de6b
sharkzeeh <igorbabikov24@gmail.com>
2024-04-03 18:04:55 +0300

    update file

--- Work/5/mro.py
+++ Work/5/mro.py
@@ -3,21 +3,29 @@ Python uses cooperative multiple inheritance which obeys some rules about class 
 
 1. Children are always checked before parents
 2. Parents (if multiple) are always checked in the order listed
+
+    object
+  /        \
+A           B
+  \      /    \
+      C        D
+        \     /
+           E
 '''
 
-class A: pass
-class B:
-    attr = 'B'
-class C(A, B): pass
-class D(B):
-    attr = 'D'
-class E(C, D): pass
+
+
+class A:        pass
+class B:        attr = 'B'
+class C(A, B):  pass
+class D(B):     attr = 'D'
+class E(C, D):  pass
 
 
 if __name__ == '__main__':
     e = E()
-    print('E bases:', E.__bases__)
-    print(f'E mro:', E.__mro__)
+    print('E bases:', E.__bases__)  # C, D
+    print(f'E mro:', E.__mro__)     # E, C, A, D, B
     for cls in e.__class__.__mro__:
         if 'attr' in cls.__dict__:
             print(f'attr was found in class {cls.__name__}!')
